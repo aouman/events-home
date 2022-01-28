da43c5bac5edd53eb39a0d2fbab0677a3f52f33f
aouman <k2.aouman@gmail.com>
2022-01-28 16:44:44 +0000

    création de l'aperçu de la facture

--- js-doc/app.js
+++ js-doc/app.js
@@ -61,9 +61,9 @@ $(document).ready(function(){
         if (input.files && input.files[0]) {
           var reader = new FileReader();
           reader.onload = function(e) {
-            $('#image-preview').attr('src', e.target.result);
-            $('#image-preview').hide();
-            $('#image-preview').fadeIn(650);
+            $('.image-preview').attr('src', e.target.result);
+            $('.image-preview').hide();
+            $('.image-preview').fadeIn(650);
           }
           reader.readAsDataURL(input.files[0]);
         }
