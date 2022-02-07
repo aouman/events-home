b7f98c8daf84175c7f319b667bdeb60535c6816b
aouman <k2.aouman@gmail.com>
2022-02-07 22:20:44 +0000

    Création de la page vue des présentation

--- js-doc/app.js
+++ js-doc/app.js
@@ -47,15 +47,6 @@ $(document).ready(function(){
           $(this).next('.custom-file-label').html(fileName);
       })
 
-      //change devise
-
-      $('#select_box').change(function () {
-        var select=$(this).find(':selected').val();
-         $(".inv_d span").hide();
-         $('.' + select).show();
-
-	    }).change();
-
 
         // Change payement block
         $("#mobile").on('click', function(){
@@ -79,7 +70,7 @@ $(document).ready(function(){
             })
         });
 
-        //add nombre de salle
+        //Ajouter le nombre de salle
 
         //onchange of rooms-count
        $('#search_rooms_select').change(function() {
@@ -232,31 +223,283 @@ $(document).ready(function(){
                });
 
 
-        //Création de tache
+        //CREATION DE TACHE
+        // A- Céation de catégorie
 
-        $('#button').click(
-            function(){
-                var toAdd = $('input[name=ListItem]').val();
-                 $('ol').append('<li>' + toAdd + '</li>');
-            });
+        $("#addCat").click(function() {
+          let value = $("#task_category_value").val();
 
-           $("input[name=ListItem]").keyup(function(event){
-              if(event.keyCode == 13){
-                $("#button").click();
-              }
+          // If input is empty, exit
+          if (value == "") {
+            return false;
+          }
+
+          // Add item to list
+          let newTaskCategory =  `<div class="card text-center mb-3 ms-3 task2_category" id="category">
+                              <button type="button" class="btn-close bg-white shadow-sm fs-6 float-end delete_category position-absolute"></button>
+                              <div class="card-header bg-color shadow-sm text-white fw-bold fs-5 text-start border-0 text-uppercase">
+                                ${value}
+                              </div>
+                              <div class="card-body px-2" id="task_box">
+
+                              </div>
+                              <div class="card-footer border-0">
+                                <button type="button" name="button" class="btn bg-color text-white fw-bold" data-bs-toggle="modal" data-bs-target="#taskModal">
+                                  Ajouter une tache
+                                </button>
+                              </div>
+                          </div>`;
+
+          $("#task_category").append(newTaskCategory);
+
+          $("#task_category").children().last().fadeIn(300);
+
+          // Clear input once created new item
+          $("#task_category_value").val("");
+        });
+
+        // 'click()' won't target newly created elements - must use 'on()'
+        $(document.body).on('click', '.delete_category', function() {
+          $(this).parent().fadeOut(300, function(){
+            $(this).remove();
+          });
+        });
+
+        // B- Céation de Tache
+
+        $("#addTask").click(function() {
+          let value_user = $("#task_user_value").val();
+          let value_name = $("#task_name_value").val();
+          let value_date = $("#task_date_value").val();
+
+          // If input is empty, exit
+          if (value_user == ""){
+            return false;
+          } else if (value_name == "") {
+            return false;
+          }else if (value_date == "") {
+            return false;
+          }
+
+
+          // Add item to list
+          let newTask =`<div class="card shadow-sm border-white mb-3" id="task_item">
+                            <div class="card-body">
+                              <div class="">
+                                <p>
+                                  <span class="me-3">
+                                    <i class="bi bi-person-circle"></i>
+                                  </span>
+                                ${value_user}
+                                </p>
+                              </div>
+                              <div class="">
+                                <p>${value_name}</p>
+                              </div>
+                              <div class="">
+                                <p>${value_date}</p>
+                              </div>
+                            </div>
+                          </div>`;
+
+          $("#task_box").append(newTask);
+          $("#task_box").children().last().fadeIn(300);
+
+          // Clear input once created new item
+          $("#task_user_value").val("");
+          $("#task_name_value").val("");
+          $("#task_date_value").val("");
+        });
+
+
+
+
+        // 'click()' won't target newly created elements - must use 'on()'
+        /*$(document.body).on('click', '.delete_category', function() {
+          $(this).parent().fadeOut(300, function(){
+            $(this).remove();
+          });
+        });*/
+
+
+        //add category for inventory
+        let cloneCount = 2;
+         $("#addInventory").click(function(){
+            $('#inventorys')
+                .clone()
+                .attr("id", "inventorys-"+ cloneCount++)
+                .insertAfter($('[id^=inventorys]:last'));
+         });
+
+         //Add catégory stock
+               let num = 0; // var num = 0;
+                function insertTemplate(){
+                  const parent = document.querySelector('#inventoryBox'); // var parent = $('#parent');
+                  const template=`<p id="paragraph${num}">${num}</p>`; // var template = $('<p id="paragraph' + num +'"></p>').text(num);
+                  num += 1;
+                  parent.appendChild(template); // $(parent).append(template)
+                }
+
+                var stockCount = 0;
+                var updatedStock = Number($(".opening.balance").html()) == "NaN" ? 0 : Number($(".opening.balance").html());
+
+                $("tr.product").each(function(index) {
+                  stockCount = 0;
+
+                  var entry = Number($("td.entry", $(this)).html());
+                  var exit = Number($("td.exit", $(this)).html());
+
+                  entry = entry == "NaN" ? 0 : entry;
+                  exit = exit == "NaN" ? 0 : exit;
+
+                  var stock = updatedStock + entry - exit;
+                  updatedStock = stock;
+
+                  stockCount += stock;
+                  $("td.stock", $(this)).html(stock);
+                });
+
+
+
+                 $("#addInventory").click(function() {
+                   let value_inventory_category = $("#inventory_total_category").val();
+
+                   // If input is empty, exit
+                   if (value_inventory_category == "") {
+                     return false;
+                   }
+
+                   // Add item to list
+                   let newinventoryCategory =  `<div class="card mb-3" id="inventorys">
+                     <div class="d-flex align-items-center justify-content-between p-2 bg-color text-white fw-bold fs-5">
+                       <p class="mb-0 d-flex align-items-center w-75" >
+                         <span class="me-3 btn shadow border-white text-white"><i class="bi bi-pencil-square"></i></span>
+                         ${value_inventory_category}
+                       </p>
+                       <p class="mb-0 btn border rounded-circle shadow bd-color text-white">x</p>
+                     </div>
+                     <div class="card-body">
+                       <table class="table table-striped">
+                         <thead>
+                           <tr>
+                             <th scope="col">Date</th>
+                             <th scope="col">Nom</th>
+                             <th scope="col">Entré</th>
+                             <th scope="col">Sortie</th>
+                             <th scope="col">Total disponible</th>
+                             <th scope="col">Action</th>
+                           </tr>
+                         </thead>
+                         <tbody class="inventory-box">
+
+                         </tbody>
+                       </table>
+                     </div>
+                   </div>`;
+
+                   $("#inventoryBox").append(newinventoryCategory);
+
+
+                   // Clear input once created new item
+                 });
+
+                 // 'click()' won't target newly created elements - must use 'on()'
+                /* $(document.body).on('click', '.delete_category', function() {
+                   $(this).parent().fadeOut(300, function(){
+                     $(this).remove();
+                   });
+                 });*/
+
+
+         //add inventory item
+         $("#addUnit").click(function() {
+           let value_inventory_date = $("#inventory_date_value").val();
+           let value_inventory_name = $("#inventory_name_value").val();
+           let value_inventory_entry = $("#inventory_entry_value").val();
+           let value_inventory_exit = $("#inventory_exit_value").val();
+           let value_inventory_total = $("#inventory_total_value").val();
+
+
+           // If input is empty, exit
+           if (value_inventory_date == ""){
+             return false;
+           } else if (value_inventory_name == "") {
+             return false;
+           }else if (value_inventory_entry == "") {
+             return false;
+           }else if (value_inventory_exit == "") {
+             return false;
+           }else if (value_inventory_total == "") {
+             return false;
+           }
+
+
+           // Add item to list
+           let newInventory =` <tr>
+                                 <td>${value_inventory_date}</td>
+                                 <td>${value_inventory_name}</td>
+                                 <td>${value_inventory_entry}</td>
+                                 <td>${value_inventory_exit}</td>
+                                 <td class="stock"></td>
+                                 <td>
+                                 <span class="btn btn-success"><i class="bi bi-pencil-square"></i></span>
+                                 <span class="btn btn-danger"><i class="bi bi-trash"></i></span>
+                                 </td>
+                               </tr>`;
+
+           $(".inventory-box").append(newInventory);
+           $(".inventory-box").children().last().fadeIn(300);
+
+           // Clear input once created new item
+           $("#inventory_date_value").val("");
+           $("#inventory_name_value").val("");
+           $("#inventory_entry_value").val("");
+           $("#inventory_exit_value").val("");
+           $("#inventory_total_value").val("");
+         });
+
+
+         // 'click()' won't target newly created elements - must use 'on()'
+         /*$(document.body).on('click', '.delete_category', function() {
+           $(this).parent().fadeOut(300, function(){
+             $(this).remove();
+           });
+         });*/
+         //<p id="paragraph${num}">${num}</p>
+
+          //Clicque pour modifier
+          let $count = 0;
+
+          let $eb = $('.edit-button');
+          let $ei = $('.editable-input');
+          let $ec = $('.editable-cell');
+
+          //Editable input fields
+          $eb.on('click', function() {
+            $count++
+            if ($count < 2) {
+              $ei.prop('readonly', false).focus();
+              $ei.prop('placeholder','');
+              $ei.val('');
+              $(this).addClass('hide');
+              $ec.addClass('editing');
+            } else {
+              $ei.prop('readonly', false).focus();
+              $eb.addClass('hide');
+              $ec.addClass('editing');
+            }
           });
 
-          $(document).on('dblclick','li', function(){
-            $(this).toggleClass('strike').fadeOut('slow');
+
+          $ei.on('blur', function() {
+            $eb.removeClass('hide');
+            $ei.prop('readonly', true);
+            $ec.removeClass('editing');
           });
 
-          $('input').focus(function() {
-            $(this).val('');
-          });
-
-          $('ol').sortable(); 
 
     });
+
 
 // copy url btn
   (function() {
